92dda7ee9b962865848a25cbed5a04462ad4ff5b
Giorgio Bianchini <giorgio@Giorgios-MacBook-Pro.local>
2022-07-27 00:57:46 +0100

    Fixed build on mac-x64.

--- native/build.sh
+++ native/build.sh
@@ -33,19 +33,23 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Building mac-x64 native library..." ;
         echo ;
         cd rapidNJWrapper/thirdparty/rapidNJ ;
-		make clean ;
-		make ;
-		cd ../../../ ;
-		rm -rf out/build/mac-x64 ;
-		mkdir -p out/build/mac-x64
+        make clean ;
+        make ;
+        cd ../../../ ;
+        rm -rf out/build/mac-x64 ;
+        mkdir -p out/build/mac-x64
         cd out/build/mac-x64 ;
         cmake ../../../ ;
         make ;
     elif [[ "$architecture" == "arm64" ]]; then
         echo "Building mac-arm64 native library..." ;
         echo ;
-		rm -rf out/build/mac-arm64 ;
-		mkdir -p out/build/mac-arm64
+        cd rapidNJWrapper/thirdparty/rapidNJ ;
+        make clean ;
+        make ;
+        cd ../../../ ;
+        rm -rf out/build/mac-arm64 ;
+        mkdir -p out/build/mac-arm64
         cd out/build/mac-arm64 ;
         cmake ../../../ ;
         make ;
